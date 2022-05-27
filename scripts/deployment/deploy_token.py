b309470c6d953f20229b38e55a30e3f5743df2f6
p7m <perpetuum7@protonmail.com>
2022-05-27 17:42:07 +0200

    feat: test changing minter addresses

--- scripts/deployment/deploy_token.py
+++ scripts/deployment/deploy_token.py
@@ -9,11 +9,19 @@ from brownie import (
 )
 
 
-ADDRESSES_EMISSION = [] # 1
-ADDRESSES_TREASURY = ["0x559dBda9Eb1E02c0235E245D9B175eb8DcC08398"] # 1
-ADDRESSES_TEAM_MEMBERS = ["0xC4d54E7e94B68d88Ad7b00d0689669d520cce2fB", "0xC983Ebc9dB969782D994627bdfFeC0ae6efee1b3"] # 4
-ADDRESSES_ADVISORS = ["0x9D097697e5B82a52f6b82c16c148a753bBF5C8ae"] # 2
-ADDRESSES_BOBA_BAR = [] # 1
+ADDRESSES_EMISSION = []  # 1
+ADDRESSES_TREASURY = ["0x559dBda9Eb1E02c0235E245D9B175eb8DcC08398"]  # 1
+ADDRESSES_TEAM_MEMBERS = [
+    "0xC4d54E7e94B68d88Ad7b00d0689669d520cce2fB",  # p7m
+    "0xC983Ebc9dB969782D994627bdfFeC0ae6efee1b3",  # eD
+    "0xbb6cBCa48EF0e6AD1110AAf866eeF4410252BD5B",  # BR
+    "0x0ad4f3762E14E6183E7b67f37C3DF53FAbfCa532"  # Zibro
+]  # 4
+ADDRESSES_ADVISORS = [
+    "0x9D097697e5B82a52f6b82c16c148a753bBF5C8ae",  # G
+    "0x03b007DD9C84153C233CD2A5f6bf7A6242f9E291",  # M
+]  # 2
+ADDRESSES_BOBA_BAR = []  # 1
 
 
 def live_part_one():
@@ -27,9 +35,7 @@ def live_part_two():
     token = Koyo.at(deployments["Koyo"])
     voting_escrow = VotingEscrow.at(deployments["VotingEscrow"])
 
-    deploy_part_two(
-        token, voting_escrow, config.tx_params, config.DEPLOYMENTS_JSON
-    )
+    deploy_part_two(token, voting_escrow, config.tx_params, config.DEPLOYMENTS_JSON)
 
 
 def deploy_part_one(_tx_params, deployments_json=None):
@@ -64,19 +70,21 @@ def deploy_part_two(token, voting_escrow, _tx_params, deployments_json=None):
         ADDRESSES_TEAM_MEMBERS,
         ADDRESSES_ADVISORS,
         ADDRESSES_BOBA_BAR,
-        _tx_params(5_000_000)
+        _tx_params(5_000_000),
     )
 
     token.set_minter(minter, _tx_params(5_000_000))
 
-    voting_escrow.commit_smart_wallet_checker(smart_wallet_whitelist, _tx_params(5_000_000))
+    voting_escrow.commit_smart_wallet_checker(
+        smart_wallet_whitelist, _tx_params(5_000_000)
+    )
     voting_escrow.apply_smart_wallet_checker(_tx_params(5_000_000))
 
     deployments = {
         "Koyo": token.contract_address,
         "VotingEscrow": voting_escrow.contract_address,
         "Minter": minter.contract_address,
-        "SmartWalletWhitelist": smart_wallet_whitelist.contract_address
+        "SmartWalletWhitelist": smart_wallet_whitelist.contract_address,
     }
 
     if deployments_json is not None:
---
+++ tests/integration/Minter/test_changing_addresses.py
@@ -0,0 +1,55 @@
+def test_voting_powers(accounts, koyo, Minter):
+    a0 = accounts[0]
+
+    initial_emission = [accounts[2]]
+    initial_treasury = [accounts[3]]
+    initial_team_members = accounts[4:8]
+    initial_advisors = accounts[8:10]
+    initial_boba_bar = [accounts[10]]
+
+    minter = Minter.deploy(koyo, initial_emission, initial_treasury, initial_team_members, initial_advisors, initial_boba_bar, {"from": a0})
+
+    for ac in range(len(initial_emission)):
+        assert minter.addresses_emission(ac) == initial_emission[ac]
+    for ac in range(len(initial_treasury)):
+        assert minter.addresses_treasury(ac) == initial_treasury[ac]
+    for ac in range(len(initial_team_members)):
+        assert minter.addresses_team_members(ac) == initial_team_members[ac]
+    for ac in range(len(initial_advisors)):
+        assert minter.addresses_advisors(ac) == initial_advisors[ac]
+    for ac in range(len(initial_boba_bar)):
+        assert minter.addresses_boba_bar(ac) == initial_boba_bar[ac]
+
+    changed_emission = [accounts[11]]
+    changed_treasury = [accounts[12]]
+    changed_team_members = accounts[13:17]
+    changed_advisors = accounts[17:19]
+    changed_boba_bar = [accounts[19]]
+
+    minter.set_addresses(changed_emission, changed_treasury, changed_team_members, changed_advisors, changed_boba_bar, {"from": a0})
+
+    for ac in range(len(changed_emission)):
+        assert minter.addresses_emission(ac) == changed_emission[ac]
+    for ac in range(len(changed_treasury)):
+        assert minter.addresses_treasury(ac) == changed_treasury[ac]
+    for ac in range(len(changed_team_members)):
+        assert minter.addresses_team_members(ac) == changed_team_members[ac]
+    for ac in range(len(changed_advisors)):
+        assert minter.addresses_advisors(ac) == changed_advisors[ac]
+    for ac in range(len(changed_boba_bar)):
+        assert minter.addresses_boba_bar(ac) == changed_boba_bar[ac]
+
+    single_changed_emission = [accounts[20]]
+
+    minter.set_addresses(single_changed_emission, changed_treasury, changed_team_members, changed_advisors, changed_boba_bar, {"from": a0})
+
+    for ac in range(len(single_changed_emission)):
+        assert minter.addresses_emission(ac) == single_changed_emission[ac]
+    for ac in range(len(changed_treasury)):
+        assert minter.addresses_treasury(ac) == changed_treasury[ac]
+    for ac in range(len(changed_team_members)):
+        assert minter.addresses_team_members(ac) == changed_team_members[ac]
+    for ac in range(len(changed_advisors)):
+        assert minter.addresses_advisors(ac) == changed_advisors[ac]
+    for ac in range(len(changed_boba_bar)):
+        assert minter.addresses_boba_bar(ac) == changed_boba_bar[ac]
