4d6d4d76489e51c9fe8302b8846aa50aadf0d8f0
p7m <perpetuum7@protonmail.com>
2022-06-06 11:32:12 +0200

    refactor: minter fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,35 @@
 import pytest
 
 
+# region Minter - accounts
+@pytest.fixture(scope="session")
+def minter_initial_emissions(accounts):
+    yield [accounts[2]]
+
+
+@pytest.fixture(scope="session")
+def minter_initial_treasury(accounts):
+    yield [accounts[3]]
+
+
+@pytest.fixture(scope="session")
+def minter_initial_team_members(accounts):
+    yield accounts[4:8]
+
+
+@pytest.fixture(scope="session")
+def minter_initial_advisors(accounts):
+    yield accounts[8:10]
+
+
+@pytest.fixture(scope="session")
+def minter_initial_boba_bar(accounts):
+    yield [accounts[10]]
+
+
+# endregion Minter - accounts
+
+
 @pytest.fixture(scope="module")
 def smart_wallet_whitelist(SmartWalletWhitelist, accounts):
     yield SmartWalletWhitelist.deploy({"from": accounts[0]})
@@ -15,6 +44,28 @@ def koyo(Koyo, accounts):
 def voting_escrow(VotingEscrow, accounts, koyo):
     yield VotingEscrow.deploy(
         koyo, "Voting-escrowed KYO", "veKYO", "veKOYO_1", {"from": accounts[0]}
+    )
+
+
+@pytest.fixture(scope="module")
+def minter(
+    Minter,
+    koyo,
+    minter_initial_emissions,
+    minter_initial_treasury,
+    minter_initial_team_members,
+    minter_initial_advisors,
+    minter_initial_boba_bar,
+    accounts,
+):
+    yield Minter.deploy(
+        koyo,
+        minter_initial_emissions,
+        minter_initial_treasury,
+        minter_initial_team_members,
+        minter_initial_advisors,
+        minter_initial_boba_bar,
+        {"from": accounts[0]},
     )
 
 
--- tests/unitary/Minter/test_changing_addresses.py
+++ tests/unitary/Minter/test_changing_addresses.py
@@ -1,24 +1,24 @@
-def test_switching_addresses(accounts, koyo, Minter):
+def test_switching_addresses(
+    accounts,
+    minter,
+    minter_initial_emissions,
+    minter_initial_treasury,
+    minter_initial_team_members,
+    minter_initial_advisors,
+    minter_initial_boba_bar,
+):
     a0 = accounts[0]
 
-    initial_emission = [accounts[2]]
-    initial_treasury = [accounts[3]]
-    initial_team_members = accounts[4:8]
-    initial_advisors = accounts[8:10]
-    initial_boba_bar = [accounts[10]]
-
-    minter = Minter.deploy(koyo, initial_emission, initial_treasury, initial_team_members, initial_advisors, initial_boba_bar, {"from": a0})
-
-    for ac in range(len(initial_emission)):
-        assert minter.addresses_emission(ac) == initial_emission[ac]
-    for ac in range(len(initial_treasury)):
-        assert minter.addresses_treasury(ac) == initial_treasury[ac]
-    for ac in range(len(initial_team_members)):
-        assert minter.addresses_team_members(ac) == initial_team_members[ac]
-    for ac in range(len(initial_advisors)):
-        assert minter.addresses_advisors(ac) == initial_advisors[ac]
-    for ac in range(len(initial_boba_bar)):
-        assert minter.addresses_boba_bar(ac) == initial_boba_bar[ac]
+    for ac in range(len(minter_initial_emissions)):
+        assert minter.addresses_emission(ac) == minter_initial_emissions[ac]
+    for ac in range(len(minter_initial_treasury)):
+        assert minter.addresses_treasury(ac) == minter_initial_treasury[ac]
+    for ac in range(len(minter_initial_team_members)):
+        assert minter.addresses_team_members(ac) == minter_initial_team_members[ac]
+    for ac in range(len(minter_initial_advisors)):
+        assert minter.addresses_advisors(ac) == minter_initial_advisors[ac]
+    for ac in range(len(minter_initial_boba_bar)):
+        assert minter.addresses_boba_bar(ac) == minter_initial_boba_bar[ac]
 
     changed_emission = [accounts[11]]
     changed_treasury = [accounts[12]]
@@ -26,7 +26,14 @@ def test_switching_addresses(accounts, koyo, Minter):
     changed_advisors = accounts[17:19]
     changed_boba_bar = [accounts[19]]
 
-    minter.set_addresses(changed_emission, changed_treasury, changed_team_members, changed_advisors, changed_boba_bar, {"from": a0})
+    minter.set_addresses(
+        changed_emission,
+        changed_treasury,
+        changed_team_members,
+        changed_advisors,
+        changed_boba_bar,
+        {"from": a0},
+    )
 
     for ac in range(len(changed_emission)):
         assert minter.addresses_emission(ac) == changed_emission[ac]
@@ -41,7 +48,14 @@ def test_switching_addresses(accounts, koyo, Minter):
 
     single_changed_emission = [accounts[20]]
 
-    minter.set_addresses(single_changed_emission, changed_treasury, changed_team_members, changed_advisors, changed_boba_bar, {"from": a0})
+    minter.set_addresses(
+        single_changed_emission,
+        changed_treasury,
+        changed_team_members,
+        changed_advisors,
+        changed_boba_bar,
+        {"from": a0},
+    )
 
     for ac in range(len(single_changed_emission)):
         assert minter.addresses_emission(ac) == single_changed_emission[ac]
